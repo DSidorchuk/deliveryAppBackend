fe6ad193670080ed45dafaed85e8b53eccfd522a
Daniil <sidorchuk.daniil@gmail.com>
2023-03-24 21:29:47 +0200

    Commit 5. Set headers to response

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.get("/shops", (req, res) => {
     const content = fs.readFileSync(filePath, "utf8");
     const contentList = JSON.parse(content);
     res.append("Access-Control-Allow-Origin", '*');
+    res.append("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.send(contentList['shops']);
 });
 
