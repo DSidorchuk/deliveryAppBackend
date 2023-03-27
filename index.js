15c018f40c3b5ad60169146a7ec52db67d17b86b
Daniil <sidorchuk.daniil@gmail.com>
2023-03-27 21:33:50 +0300

    Commit 14. Re-wrote all inquiry handlers as urls were awfaul

--- index.js
+++ index.js
@@ -17,21 +17,21 @@ const corsOptions = {
 };
 
 // Get list of shops for main page 
-app.options('/shops', cors());
-app.get("/shops", cors(corsOptions), (req, res) => {
+app.options('/shopslist', cors());
+app.get("/shopslist", cors(corsOptions), (req, res) => {
     res.send(contentList['shops']);
 });
 
 // Get list of products in according to choosen shop
-app.options("/:shopId", cors());
-app.get("/:shopId", cors(corsOptions), (req, res) => {
-    const shopId = req.params["shopId"];
+app.options("/products", cors());
+app.get("/products", cors(corsOptions), (req, res) => {
+    const shopId = req.query.shop;
     res.send(contentList[shopId]);
 });
 
 // Get data of single product by it`s shop and id
-app.options("/:shopId/:itemId", cors());
-app.get("/:shopId/:itemId", cors(corsOptions), (req, res) => {
+app.options("/product/:shopId/:itemId", cors());
+app.get("/product/:shopId/:itemId", cors(corsOptions), (req, res) => {
     const shopId = req.params["shopId"];
     const itemId = req.params["itemId"];
     contentList[shopId].forEach(item => {
@@ -44,15 +44,15 @@ app.get("/:shopId/:itemId", cors(corsOptions), (req, res) => {
 // Get list of coupones for coupones page
 app.options('/coupones', cors());
 app.get('/coupones', cors(corsOptions), (req, res) => {
-    res.send(contentList['/coupones']);
+    res.send(contentList['coupones']);
 });
 
-app.options("/orders/:phone", cors());
-app.get("/orders/:phone", cors(corsOptions), (req, res) => {
-    const phone = req.params['phone'];
-    const orders = contentList['orders'];
-    const userOrders = orders.filter(item => item.phone === phone );
-    res.send(userOrders);
+// Get orders list by phone
+app.options("/history/:phone", cors());
+app.get("/history/:phone", cors(corsOptions), (req, res) => {
+    const phone = req.params["phone"];
+    const orders = contentList['orders'].filter(item => item.phone === phone);
+    res.send(orders);
 });
 
 app.listen(80);
